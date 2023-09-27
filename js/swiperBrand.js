8450bdc5168e45165c66279eed118e5fdec461f7
Danila Kanapatski <danila_konopatskiy@mail.ru>
2023-09-27 23:42:01 +0300

    fixed all pages

---
+++ js/swiperBrand.js
@@ -0,0 +1,44 @@
+new Swiper('.image-slider1', {
+    speed: 700,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.image-slider1 .swiper-button-next',
+        prevEl: '.image-slider1 .swiper-button-prev',
+    },
+    slideToClickedSlide: true,
+    keyboard: {
+        enabled: true
+    }, 
+    // autoHeight: true,
+    // loop: true,
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnInteraction: false,
+    // },
+    slidesPerView: 'auto',
+    slidesPerGroup: 1,
+});
+
+new Swiper('.image-slider2', {
+    speed: 700,
+    spaceBetween: 23,
+    navigation: {
+        nextEl: '.image-slider2 .swiper-button-next',
+        prevEl: '.image-slider2 .swiper-button-prev',
+    },
+    slideToClickedSlide: true,
+    keyboard: {
+        enabled: true
+    }, 
+    // autoHeight: true,
+    // loop: true,
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnInteraction: false,
+    // },
+    slidesPerView: 'auto',
+    slidesPerGroup: 1,
+});
+
+
+  
